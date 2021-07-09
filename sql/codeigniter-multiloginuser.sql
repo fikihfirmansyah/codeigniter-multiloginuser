7e98da741a138c947389d7397d16103e3bdb05e6
Fikih Firmansyah <fikihfirmansyah43@gmail.com>
2021-07-10 00:56:07 +0700

    update

--- sql/codeigniter-multiloginuser.sql
+++ sql/codeigniter-multiloginuser.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 08, 2021 at 07:09 PM
+-- Generation Time: Jul 09, 2021 at 07:55 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.4.16
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pl4`
+-- Database: `codeigniter-multiloginuser`
 --
 
 -- --------------------------------------------------------
@@ -82,9 +82,8 @@ CREATE TABLE `tbl_user` (
   `password` varchar(100) NOT NULL,
   `password_reset_key` varchar(100) DEFAULT NULL,
   `name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `phone` varchar(12) NOT NULL,
+  `phone` varchar(15) NOT NULL,
   `photo` varchar(100) NOT NULL,
   `activated` tinyint(1) NOT NULL DEFAULT 0,
   `last_login` datetime DEFAULT NULL,
@@ -96,14 +95,9 @@ CREATE TABLE `tbl_user` (
 -- Dumping data for table `tbl_user`
 --
 
-INSERT INTO `tbl_user` (`id`, `id_role`, `username`, `password`, `password_reset_key`, `name`, `last_name`, `email`, `phone`, `photo`, `activated`, `last_login`, `created_at`, `updated_at`) VALUES
-(1, 1, 'admin', '$2y$05$OA.OoeNHoEkbGGKazYqPU.UOaI5jmgro8x2pRSV56ClTWlDf0EEn2', '', 'SISTEM', 'ADMINISTRATOR', 'admin@administrator.com', '082370382008', '1625761991343.jpg', 1, '2021-07-08 21:31:06', '2021-07-08 23:53:53', NULL),
-(2, 2, 'member', '$2y$05$8GdJw3BVbmhN6x2t0MNise7O0xqLMCNAN1cmP6fkhy0DZl4SxB5iO', '', 'Cabang Brandan', 'Kota', 'fikihfirmansyah43@gmail.com', '082370382008', '1625763396205.jpg', 1, '2021-07-08 23:56:50', '2021-07-08 23:53:53', NULL),
-(5, 2, 'fikihfirmansyah43@qmail.id', '$2y$05$4GwizExaZIQivMmh7Uc1BOwe7cz177WlhqVql4BDUNuvwzO3P/UHC', NULL, '', '', 'fikihfirmansyah43@qmail.id', '', '', 0, NULL, '2021-07-08 23:58:01', NULL),
-(6, 2, 'admin@example.com', '$2y$05$K3wqgV6fFH.NCN7RZMgDIOGCchTm5iaUvVzuYEUvl6kbQ8uhL3HD2', NULL, '', '', 'admin@example.com', '', '', 0, NULL, '2021-07-08 23:58:37', NULL),
-(7, 2, 'admin@admin.com', '$2y$05$ZLSn1MpCCamJkL6RodeAaOv9gyp1X0ChG7WtSyBKlrU37Kdnhpmm2', NULL, '', '', 'admin@admin.com', '', '', 0, NULL, '2021-07-09 00:00:19', NULL),
-(8, 2, 'fikihfirmansyah43@gmail.com', '$2y$05$CfKy1rUtFbHwHlWCvByISOEick3TlpLh7mH6TgCcR8LwfxxjJuL9W', NULL, '', '', 'fikihfirmansyah43@gmail.com', '', '', 0, NULL, '2021-07-09 00:00:47', NULL),
-(9, 2, 'united-erasmus@usu.ac.id', '$2y$05$4KMMvliPpmjJqfcRB/bgVOBoXIcF.ZJA2/CtPPjU717gsHxvGoHJ2', NULL, '', '', 'united-erasmus@usu.ac.id', '', '', 0, NULL, '2021-07-09 00:01:06', NULL);
+INSERT INTO `tbl_user` (`id`, `id_role`, `username`, `password`, `password_reset_key`, `name`, `email`, `phone`, `photo`, `activated`, `last_login`, `created_at`, `updated_at`) VALUES
+(1, 1, 'admin', '$2y$05$OA.OoeNHoEkbGGKazYqPU.UOaI5jmgro8x2pRSV56ClTWlDf0EEn2', '', 'SISTEM', 'admin@administrator.com', '082370382008', '1625761991343.jpg', 1, '2021-07-10 00:44:32', '2021-07-08 23:53:53', NULL),
+(2, 2, 'member', '$2y$05$8GdJw3BVbmhN6x2t0MNise7O0xqLMCNAN1cmP6fkhy0DZl4SxB5iO', '', 'Cabang Brandan', 'fikihfirmansyah43@gmail.com', '082370382008', '1625763396205.jpg', 1, '2021-07-08 23:56:50', '2021-07-08 23:53:53', NULL);
 
 --
 -- Indexes for dumped tables
@@ -147,7 +141,7 @@ ALTER TABLE `tbl_role`
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
